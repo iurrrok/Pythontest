7fe1d486c10cd6ec6d96e7d05516a0e21a2d997b
Serghei Sadovnic <murderofall@gmail.com>
2019-07-03 21:29:22 +0300

    improve some optimization

--- Klick1.py
+++ Klick1.py
@@ -1,82 +1,24 @@
-#Импортирование библиотек
+# -*- coding: utf-8 -*-
 
-import pyautogui #отвечает за АвтоКликер
-import time  #отвечает за Отсчет
-
-#Отсчет(необязателен)
+import pyautogui
+import time
 
 print("Через 6 секунд запустится АвтоКликер!!!")
-print(1)
-time.sleep(0.50)
-print(2)
-time.sleep(1)
-print(3)
-time.sleep(1.50)
-print(4)
-time.sleep(2)
-print(5)
-time.sleep(2.50)
-print(6)
-time.sleep(3)
+for x in range(6):
+    print(x+1)
+    time.sleep(1)
+else:
+    print("============================")
+    print("АвтоКликер запустился!")
 
-print("============================")
-print("АвтоКликер запустился!")
 
-#Цикл
+def action():
+    pyautogui.moveTo(500, 650, duration=2)
+    pyautogui.click(interval=10)  # waiting 10 seconds
+    pyautogui.click()
+    time.sleep(1)  # waiting 1 sec
+    pyautogui.hotkey('ctrl', 'w')  # close window
 
-#while 1 == 1:
-#  pyautogui.click(interval=10)# делает клик
-#   pyautogui.press('f5',interval=50)# делает рефреш
 
-#   бот будет листать несколько страниц
-def multiply(x,y):
-	pyautogui.press('f5')# делает рефреш
-	for test in range(x):
-		pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('enter')# делает рефреш
-	pyautogui.moveTo(500, 650, duration=2)
-	pyautogui.click(interval=100) ##########не делает клик
-	pyautogui.click ##########тоже не делает клик,хотя в другом файле работает нормально
-	pyautogui.hotkey('ctrl', 'w')
-# 2
-#while 1 == 1:
-multiply(18,1)	
-multiply(19,1)
-multiply(20,1)
-multiply(21,1)
-multiply(22,1)
-multiply(23,1)
-multiply(24,1)
-multiply(25,1)
-multiply(26,1)
-multiply(27,1)
-multiply(28,1)
-multiply(29,1)
-multiply(30,1)
-multiply(31,1)
-multiply(32,1)
-multiply(33,1)
-'''
-	pyautogui.press('f5')# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш			
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('tab',interval=2)# делает рефреш
-	pyautogui.press('enter')# делает рефреш
-	pyautogui.click(500, 650, interval=20)
-	pyautogui.hotkey('ctrl', 'w')
-	'''
-	
+action()
+print("Fin!")
